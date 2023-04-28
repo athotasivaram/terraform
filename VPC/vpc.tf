ef19bb0691fd3eaba22dc5a5b72aba66ecfed04f
sivaram <athotasivaram@gmail.com>
2023-04-28 16:24:06 +0530

    vpc terraform

--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -33,3 +33,15 @@ resource "aws_internet_gateway" "automated-igw" {
   }
 }
 
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.automated-igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
